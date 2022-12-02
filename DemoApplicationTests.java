ea9f1ec23ab6e54109c7154267103052b930089c
lyseebean <112133626+srfehr@users.noreply.github.com>
2022-12-01 20:43:11 -0600

    tests

---
+++ DemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+		Greeting greeting = new Greeting(1, "Test");
+		assertEquals("Test", greeting.getContent());
+	}
+
+}
